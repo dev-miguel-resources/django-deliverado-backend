b5505ed0d2ace20d6281f8d07fd821effaed2fa5
HectorArteaga <hector12arteaga@gmail.com>
2021-04-29 10:18:44 -0500

    third commit

---
+++ dapi/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from dapi.settings import local
+from django.conf.urls.static import static
+from dapi.settings import base
+from django.conf.urls.static import static
+
+urlpatterns = [
+    
+	#custom dapi
+	path('api/v1/', include('apps.api.urls')),
+	path('cart/', include('apps.cart.urls')),
+	path('dashboard/', include('apps.dashboard.urls')),
+	path('master/', include('apps.master.urls')),
+	path('users/', include('apps.users.urls')),
+	path('reset_password/', include('apps.drp.urls')),
+	#default
+    path('admin/', admin.site.urls),
+
+]+ static(base.MEDIA_URL, document_root=base.MEDIA_ROOT)
